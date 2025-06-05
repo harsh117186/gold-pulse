3bea6cfd3f5325445bf39a6545efc7cbcc5c0457
harsh117186 <harsh.parekh117186@marwadiuniversity.ac.in>
2025-06-05 17:49:54 +0530

    Optimize build process for Render deployment

--- webpack.config.js
+++ webpack.config.js
@@ -8,21 +8,38 @@ module.exports = {
   mode: 'production',
   optimization: {
     minimize: true,
+    removeAvailableModules: true,
+    removeEmptyChunks: true,
+    mergeDuplicateChunks: true,
+    flagIncludedChunks: true
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+            happyPackMode: true
+          }
+        },
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
+    cacheWithContext: false
   },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename]
+    }
+  }
 }; 
