5cc466598c100b612ee6be3fee5afa019a0589e6
harsh117186 <harsh.parekh117186@marwadiuniversity.ac.in>
2025-06-05 15:44:44 +0530

    Initial commit1

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/main.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  mode: 'production',
+  optimization: {
+    minimize: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}; 
